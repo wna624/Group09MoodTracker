5d6fbb6c26e7ffca744311afcc3cdc6f0260661a
BuildTools <91291246+smurray769@users.noreply.github.com>
2021-11-11 22:52:50 -0600

    updates to MoodTracker class

--- src/application/MoodTracker.java
+++ src/application/MoodTracker.java
@@ -1,5 +1,21 @@
 package application;
 
+import java.util.HashMap;
+
 public class MoodTracker {
+	private String username;
+	private HashMap<String, Mood> moodData = new HashMap<String, Mood>(); //hashmap for storing the date and a mood object
+	//note: considering using the Java utility class Date instead of a string for the date
+	
+	//constructor
+	public MoodTracker(String username) {
+		this.username = username;
+	}
+	
+	public void addMood(String date, Mood newMood) {
+		moodData.put(date, newMood);
+	}
+	
+	//maybe we should add a way to save this data to a file with the username and then their data will be saved between sessions
 
 }
